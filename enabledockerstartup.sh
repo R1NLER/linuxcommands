8cf46660136352b3c613d1249070647231ac0e94
R1NLER <90422888+R1NLER@users.noreply.github.com>
2022-02-27 04:50:36 +0100

    Update enabledockerstartup.sh

--- enabledockerstartup.sh
+++ enabledockerstartup.sh
@@ -1,19 +1,4 @@
-sudo nano /etc/systemd/system/docker-wireguard-pihole.service
-#content
-[Unit]
-Description=Docker Compose Wireguard / Pi-Hole Service
-Requires=docker.service
-After=docker.service
-[Service]
-Type=oneshot
-RemainAfterExit=yes
-WorkingDirectory=/opt/docker/wireguard-pihole/
-ExecStart=/usr/bin/docker-compose up -d
-ExecStop=/usr/bin/docker-compose down
-TimeoutStartSec=0
-[Install]
-WantedBy=multi-user.target
-#end of content
+sudo wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-wireguard-pihole.service /etc/systemd/system/
 sudo chmod 640 /etc/systemd/system/docker-wireguard-pihole.service
 sudo systemctl daemon-reload
 sudo systemctl enable docker-wireguard-pihole.service
