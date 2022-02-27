644dad52bb5642685c98336091d3d57cf26f00f4
R1NLER <90422888+R1NLER@users.noreply.github.com>
2022-02-27 12:58:33 +0100

    Update enabledockerstartup.sh

--- enabledockerstartup.sh
+++ enabledockerstartup.sh
@@ -1,7 +1,7 @@
 cd /opt/docker/wireguard-pihole
 sudo docker-compose up -d
 sudo wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-wireguard-pihole.service 
-mv docker-wireguard-pihole.service /etc/systemd/system/docker-wireguard-pihole.service
+sudo mv docker-wireguard-pihole.service /etc/systemd/system/docker-wireguard-pihole.service
 sudo chmod 640 /etc/systemd/system/docker-wireguard-pihole.service
 sudo systemctl daemon-reload
 sudo systemctl enable docker-wireguard-pihole.service
