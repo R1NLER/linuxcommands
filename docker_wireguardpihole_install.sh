8c4d65b139b9d9c40c62f1941ad38514eeddc4a5
R1NLER <90422888+R1NLER@users.noreply.github.com>
2022-02-27 13:49:38 +0100

    Update and rename dockerinstall.sh to docker_wireguardpihole_install.sh

--- docker_wireguardpihole_install.sh
+++ docker_wireguardpihole_install.sh
@@ -6,7 +6,7 @@ sudo systemctl start docker.service
 sudo systemctl status docker.service
 sudo mkdir -p /opt/docker/wireguard-pihole
 cd /opt/docker/wireguard-pihole
-sudo wget https://github.com/R1NLER/linuxcommands/blob/main/docker-compose.yml
+wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-compose.yml
 nano /opt/docker/wireguard-pihole/docker-compose.yml
 #fill the .yml doc and exec with docker-compose up -d
 sudo sed -i "s/127.0.0.1 localhost/127.0.0.1 localhost $(hostname)/g" /etc/hosts
@@ -16,3 +16,8 @@ sudo sed -i '/nameserver 127.0.0.53/a nameserver 8.8.8.8' /etc/resolv.conf
 sudo sed -i '/nameserver 127.0.0.53/a nameserver 8.8.4.4' /etc/resolv.conf
 cd /opt/docker/wireguard-pihole
 sudo docker-compose up -d
+sudo wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-wireguard-pihole.service 
+sudo mv docker-wireguard-pihole.service /etc/systemd/system/docker-wireguard-pihole.service
+sudo chmod 640 /etc/systemd/system/docker-wireguard-pihole.service
+sudo systemctl daemon-reload
+sudo systemctl enable docker-wireguard-pihole.service
