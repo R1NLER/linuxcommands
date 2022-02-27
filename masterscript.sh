431be08c2375c7ec587fbbaf60d59b48b27c9454
R1NLER <90422888+R1NLER@users.noreply.github.com>
2022-02-27 05:35:22 +0100

    Update masterscript.sh

--- masterscript.sh
+++ masterscript.sh
@@ -34,7 +34,8 @@ sudo sed -i '/nameserver 127.0.0.53/a nameserver 8.8.8.8' /etc/resolv.conf
 sudo sed -i '/nameserver 127.0.0.53/a nameserver 8.8.4.4' /etc/resolv.conf
 cd /opt/docker/wireguard-pihole
 sudo docker-compose up -d
-sudo wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-wireguard-pihole.service /etc/systemd/system/
+sudo wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-wireguard-pihole.service 
+mv docker-wireguard-pihole.service /etc/systemd/system/docker-wireguard-pihole.service
 sudo chmod 640 /etc/systemd/system/docker-wireguard-pihole.service
 sudo systemctl daemon-reload
 sudo systemctl enable docker-wireguard-pihole.service
