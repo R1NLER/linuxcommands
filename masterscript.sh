88b94dc48389f0d09620f19717a06702f0be4259
R1NLER <90422888+R1NLER@users.noreply.github.com>
2022-02-27 04:57:39 +0100

    Update masterscript.sh

--- masterscript.sh
+++ masterscript.sh
@@ -30,3 +30,10 @@ sudo systemctl disable systemd-resolved
 sudo systemctl stop systemd-resolved
 sudo sed -i '/nameserver 127.0.0.53/a nameserver 8.8.8.8' /etc/resolv.conf
 sudo sed -i '/nameserver 127.0.0.53/a nameserver 8.8.4.4' /etc/resolv.conf
+cd /opt/docker/wireguard-pihole
+sudo docker-compose up -d
+sudo wget https://raw.githubusercontent.com/R1NLER/linuxcommands/main/docker-wireguard-pihole.service /etc/systemd/system/
+sudo chmod 640 /etc/systemd/system/docker-wireguard-pihole.service
+sudo systemctl daemon-reload
+sudo systemctl enable docker-wireguard-pihole.service
+wget -O - https://raw.githubusercontent.com/OpenMediaVault-Plugin-Developers/installScript/master/install | sudo bash
